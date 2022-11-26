dc2c116c49e6b8288a0df0b008f2d17e49222961
Christopher Guyton <ChristopherGuyton87@gmail.com>
2022-11-26 09:03:57 -0800

    Revert "Added Money When Player Does a Delivery"
    This reverts commit c9b42f073a176ec40deb51269b77f09118c75512.

--- Assets/Game/Scripts/NPCs/Deliveries/DeliveryCustomer.cs
+++ Assets/Game/Scripts/NPCs/Deliveries/DeliveryCustomer.cs
@@ -5,27 +5,20 @@ using UnityEngine;
 public class DeliveryCustomer : MonoBehaviour
 {
     public string CustomerName = ""; //Name of the customer being delivered to
-    public int tipGiven;
-
+    public Transform PositionMarker; //screen will stop here until the delivery has been made
 
     public delegate void DeliveryMade(GameObject g);
     public static event DeliveryMade onDeliveryMade;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<DeliverySystem>() != null)
+        if (collision.GetComponent<DeliverySystem>())
         {
-
           DeliverySystem getdeliveries = collision.GetComponent<DeliverySystem>();
-          CurrencySystem payment = collision.GetComponent<CurrencySystem>();
             if (getdeliveries.deliveriesLeft > 0)
             {
                 getdeliveries.MinusDeliveries(1);
-                payment.AddCurrency(tipGiven);
                 if (onDeliveryMade != null) onDeliveryMade(this.gameObject);
-
-
-
                 this.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.green; //Temporarily Turn The Delivered Persons Sprite Green
               
                 Destroy(this);
