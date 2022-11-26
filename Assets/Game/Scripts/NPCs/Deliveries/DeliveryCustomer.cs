c9b42f073a176ec40deb51269b77f09118c75512
Christopher Guyton <ChristopherGuyton87@gmail.com>
2022-11-25 21:18:17 -0800

    Added Money When Player Does a Delivery

--- Assets/Game/Scripts/NPCs/Deliveries/DeliveryCustomer.cs
+++ Assets/Game/Scripts/NPCs/Deliveries/DeliveryCustomer.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class DeliveryCustomer : MonoBehaviour
 {
     public string CustomerName = ""; //Name of the customer being delivered to
-    public Transform PositionMarker; //screen will stop here until the delivery has been made
+    public int tipGiven;
+
 
     public delegate void DeliveryMade(GameObject g);
     public static event DeliveryMade onDeliveryMade;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.GetComponent<DeliverySystem>())
+        if (collision.GetComponent<DeliverySystem>() != null)
         {
+
           DeliverySystem getdeliveries = collision.GetComponent<DeliverySystem>();
+          CurrencySystem payment = collision.GetComponent<CurrencySystem>();
             if (getdeliveries.deliveriesLeft > 0)
             {
                 getdeliveries.MinusDeliveries(1);
+                payment.AddCurrency(tipGiven);
                 if (onDeliveryMade != null) onDeliveryMade(this.gameObject);
+
+
+
                 this.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.green; //Temporarily Turn The Delivered Persons Sprite Green
               
                 Destroy(this);
