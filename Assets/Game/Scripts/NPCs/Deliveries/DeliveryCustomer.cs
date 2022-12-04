668d5e34cff9ac72388867bda880cf982d175b55
Christopher Guyton <ChristopherGuyton87@gmail.com>
2022-12-03 23:23:23 -0800

    Added Tip Ticker To UI

--- Assets/Game/Scripts/NPCs/Deliveries/DeliveryCustomer.cs
+++ Assets/Game/Scripts/NPCs/Deliveries/DeliveryCustomer.cs
@@ -10,14 +10,23 @@ public class DeliveryCustomer : MonoBehaviour
     public delegate void DeliveryMade(GameObject g);
     public static event DeliveryMade onDeliveryMade;
 
+
+
+    public delegate void TipReceivedEvent(string CustomerName, int tipToGive);
+    public static event TipReceivedEvent tipReceivedEvent;
+    public int tipToGive; // TO DO --- Adjust this based on how well the player did on the delivery!
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<DeliverySystem>())
         {
           DeliverySystem getdeliveries = collision.GetComponent<DeliverySystem>();
+            CurrencySystem giveCurrency = collision.GetComponent<CurrencySystem>();
             if (getdeliveries.deliveriesLeft > 0)
             {
                 getdeliveries.MinusDeliveries(1);
+                giveCurrency.AddCurrency(tipToGive);
+                if (tipReceivedEvent != null) tipReceivedEvent(CustomerName, tipToGive);
                 if (onDeliveryMade != null) onDeliveryMade(this.gameObject);
                 this.gameObject.GetComponentInChildren<SpriteRenderer>().color = Color.green; //Temporarily Turn The Delivered Persons Sprite Green
               
--- Assets/Game/Scripts/UI/UIDeliverySystem.cs
+++ Assets/Game/Scripts/UI/UIDeliverySystem.cs
@@ -6,16 +6,20 @@ using UnityEngine.UI;
 public class UIDeliverySystem : MonoBehaviour
 {
     public Text Deliveries;
-   
+    public Text DeliveryResultTicker;
+
+    private float tickerTime = 5f;
 
     private void OnEnable()
     {
         DeliverySystem.deliveryEvent += UpdateDeliveryUI;
+        DeliveryCustomer.tipReceivedEvent += UpdateTipTicker;
     }
 
     private void OnDisable()
     {
         DeliverySystem.deliveryEvent -= UpdateDeliveryUI;
+        DeliveryCustomer.tipReceivedEvent -= UpdateTipTicker;
     }
 
     private void Start()
@@ -25,7 +29,20 @@ public class UIDeliverySystem : MonoBehaviour
 
     public void UpdateDeliveryUI(int deliveries)
     {
-       
         Deliveries.text = "x" + deliveries;
+
+    }
+
+    public void UpdateTipTicker(string customerName, int tipReceived)
+    {
+        DeliveryResultTicker.gameObject.SetActive(true);
+        DeliveryResultTicker.text = customerName + " gave you a " + "$"+ tipReceived + " tip!";
+        StartCoroutine(RemoveTipTicker());
+    }
+
+    IEnumerator RemoveTipTicker()
+    {
+        yield return new WaitForSeconds(tickerTime);
+        DeliveryResultTicker.gameObject.SetActive(false);
     }
 }
